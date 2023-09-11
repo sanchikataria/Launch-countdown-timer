656db6653ad08f6248c61ee1a6e29ea480fc3ade
sanchi kataria <sanchikataria@sanchis-MacBook-Air.local>
2023-09-12 01:23:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+countdown=()=>{
+const endDate =new Date("May 31, 2023 00:00:00").getTime()
+const now = new Date().getTime()
+const difference = endDate-now
+
+const seconds=1000
+const minutes=seconds*60
+const hours = minutes*60
+const days= hours*24
+
+let timeDays= Math.floor(difference/days)
+let timeHours=Math.floor((difference%days)/ hours)
+let timeMinutes= Math.floor((difference%hours)/ minutes)
+let timeSeconds=Math.floor((difference%minutes)/ seconds)
+
+timeHours= timeHours< 10 ? "0"+ timeHours:timeHours
+timeMinutes= timeMinutes< 10 ? "0"+ timeMinutes:timeMinutes
+timeSeconds= timeSeconds< 10 ? "0"+ timeSeconds:timeSeconds
+
+document.getElementById("Days").innerHTML=timeDays
+document.getElementById("Hours").innerHTML=timeHours
+document.getElementById("Minutes").innerHTML=timeMinutes
+document.getElementById("Seconds").innerHTML=timeSeconds
+
+}
+setInterval(countdown ,1000)
